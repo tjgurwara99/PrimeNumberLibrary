c3aca455cc8973602f9de333144674ea1fc805e2
Taj <tjgurwara99@gmail.com>
2018-01-18 17:52:44 +0000

    Adding the AKS Primality test

--- libs/prime/prime.h
+++ libs/prime/prime.h
@@ -1,13 +1,17 @@
-#pragma once
+#ifndef _PRIME_H
+#define _PRIME_H
 #include<bitset>
 #include<boost/multiprecision/cpp_int.hpp>
+#include<boost/multiprecision/cpp_bin_float.hpp>
 #include<string>
+#include<cmath>
 #include<vector>
 #define cpp_int boost::multiprecision::cpp_int
+#define cpp_bin_float_100 boost::multiprecision::cpp_bin_float_100
 
 std::vector <cpp_int> BinaryK(cpp_int); //convertion of long to binary
 
- cpp_int ModularExpo(cpp_int , std::vector<cpp_int> &K, cpp_int n); //modular exponentiations optimised using binary
+cpp_int ModularExpo(cpp_int , std::vector<cpp_int> &K, cpp_int n); //modular exponentiations optimised using binary
 
 cpp_int GCD(cpp_int, cpp_int); // greatest common divisor
 
@@ -17,4 +21,8 @@ cpp_int modpow(cpp_int, cpp_int, cpp_int);  //modular exponentiation - binary fu
 
 bool fermatprime(cpp_int); // fermat's primality test
 
+bool isint(cpp_bin_float_100);
 
+bool perfect_pow(cpp_int);
+
+#endif
